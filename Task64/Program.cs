71dec0d8a2da8758f2a29d6836e123573651d7a2
Vasiliy Syomkin <131575484+777Sierra@users.noreply.github.com>
2023-05-10 16:47:11 +0300

    64/66/68

---
+++ Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Введите значение M:");
+        int M;
+        while (!int.TryParse(Console.ReadLine(), out M) || M < 1) {
+            Console.WriteLine("Некорректный ввод. Введите целое число больше 0:");
+        }
+        
+        Console.WriteLine("Введите значение N:");
+        int N;
+        while (!int.TryParse(Console.ReadLine(), out N) || N < M) {
+            Console.WriteLine($"Некорректный ввод. Введите целое число не меньше {M}:");
+        }
+        
+        PrintNumbersInRange(M, N);
+    }
+    
+    static void PrintNumbersInRange(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Task66/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Введите значение M:");
+        int M;
+        while (!int.TryParse(Console.ReadLine(), out M) || M < 1) {
+            Console.WriteLine("Некорректный ввод. Введите целое число больше 0:");
+        }
+        
+        Console.WriteLine("Введите значение N:");
+        int N;
+        while (!int.TryParse(Console.ReadLine(), out N) || N < M) {
+            Console.WriteLine($"Некорректный ввод. Введите целое число не меньше {M}:");
+        }
+        
+        PrintNumbersInRange(M, N);
+        int sum = GetSumOfNumbersInRange(M, N);
+        Console.WriteLine($"Сумма натуральных чисел в промежутке от {M} до {N}: {sum}");
+    }
+    
+    static void PrintNumbersInRange(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            Console.WriteLine(i);
+        }
+    }
+    
+    static int GetSumOfNumbersInRange(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
